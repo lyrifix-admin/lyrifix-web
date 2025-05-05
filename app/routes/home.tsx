25c63406bf340e76fc8e80f75c27da72a89a939b
M Haidar Hanif <git@mhaidarhanif.com>
2025-05-05 20:58:36 +0700

    feat: netlify plugin and more

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -2,15 +2,15 @@ import type { Route } from "./+types/home";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Lyrifix" },
+    { name: "description", content: "Fix the lyric, Feel the music." },
   ];
 }
 
 export default function Home() {
   return (
     <div>
-      <h1>Welcome to Lyrifix</h1>
+      <h1 className="text-2xl text-blue-950">Welcome to Lyrifix</h1>
       <p>Fix the lyric, Feel the music</p>
     </div>
   );
