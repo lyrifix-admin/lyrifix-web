2b1e36aad6664d09895c0d28347975b9a75c734b
indahmutiah <indahmutiahmz@gmail.com>
2025-05-11 14:04:04 +0700

    feat: implement search functionality with API aand update header fixed area

--- app/layouts/layout.tsx
+++ app/layouts/layout.tsx
@@ -4,10 +4,12 @@ import { BottomNavbar } from "~/components/bottom-navbar";
 
 export default function Layout() {
   return (
-    <div className="bg-background no-scrollbar relative mx-auto min-h-screen max-w-[500px] overflow-auto">
-      <Header />
+    <div className="bg-background no-scrollbar fixed relative mx-auto min-h-screen w-full max-w-[500px] overflow-auto">
+      <div className="fixed top-0 left-1/2 z-10 w-full max-w-[500px] -translate-x-1/2">
+        <Header />
+      </div>
 
-      <div className="mx-auto max-w-screen-xl px-4 pb-28 sm:px-6 md:px-8">
+      <div className="px-4 pt-16 pb-28 sm:px-6 md:px-8">
         <Outlet />
       </div>
 
---
+++ app/routes/search.tsx
@@ -0,0 +1,46 @@
+import type { Route } from "./+types/search";
+import type { SongsResponse } from "~/modules/song/type";
+import { SongCard } from "~/components/song-card";
+import { Link } from "react-router";
+export function meta({}: Route.MetaArgs) {
+  return [
+    { title: "Lyrifix | Search" },
+    {
+      name: "description",
+      content: "Search for your favorite songs and artists on Lyrifix.",
+    },
+  ];
+}
+
+export async function loader({
+  request,
+}: Route.LoaderArgs): Promise<SongsResponse> {
+  const url = new URL(request.url);
+  const keyword = url.searchParams.get("keyword") || "";
+  const response = await fetch(
+    `${process.env.BACKEND_API_URL}/songs/search?keyword=${keyword}`,
+  );
+  const songs: SongsResponse = await response.json();
+  return songs;
+}
+
+export default function SearchRoute({
+  loaderData,
+}: {
+  loaderData: SongsResponse;
+}) {
+  const songs = loaderData.songs;
+  return (
+    <div>
+      <ul className="grid grid-cols-2 gap-4">
+        {songs.map((song) => (
+          <li key={song.id}>
+            <Link to={`/songs/${song.slug}`}>
+              <SongCard song={song} />
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
