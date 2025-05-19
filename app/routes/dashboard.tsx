0714ea37e22c3a495d7ee6242b34ae9c890daa9f
Indah Mutiah MZ <indahmutiahmz@gmail.com>
2025-05-19 13:03:47 +0700

    feat : add element to song form

--- app/routes/dashboard.tsx
+++ app/routes/dashboard.tsx
@@ -1,6 +1,8 @@
 import { destroySession, getSession } from "~/sessions.server";
 import type { Route } from "./+types/dashboard";
-import { redirect } from "react-router";
+import { Link, redirect } from "react-router";
+import { Card, CardContent } from "~/components/ui/card";
+import { PlusIcon } from "lucide-react";
 
 export async function loader({ request }: Route.LoaderArgs) {
   const session = await getSession(request.headers.get("Cookie"));
@@ -35,8 +37,19 @@ export async function loader({ request }: Route.LoaderArgs) {
 export default function Dashboard({ loaderData }: Route.ComponentProps) {
   return (
     <div className="p-6 text-white">
-      <h1 className="text-2xl font-bold">Dashboard</h1>
+      <h1 className="text-2xl font-bold">Your Library</h1>
       <p className="mb-4 text-lg">Welcome back, {loaderData.fullName}!</p>
+
+      <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3">
+        <Card className="flex flex-col items-center justify-center gap-4 p-6 text-white">
+          <Link to="/add-song" className="h-full w-full">
+            <CardContent className="flex h-full flex-col items-center justify-center">
+              <PlusIcon className="h-12 w-12 text-gray-300" />
+              <span className="mt-2 text-sm font-medium text-white">Add</span>
+            </CardContent>
+          </Link>
+        </Card>
+      </div>
     </div>
   );
 }
